63617b1ad0bf4becd35ebee75a70302fbfda21e6
Aakash Bhadana <aakashbhadana@outlook.com>
2019-07-05 00:14:27 +0530

    Wrapped code into reusable functions and changed data structure to Objects
    Formatted code and wrapped reusable code into functions. Now Objects are used to store user data rather than plain strings.

--- main.js
+++ main.js
@@ -4,21 +4,20 @@ const {
     ipcMain
 } = require('electron')
 
-//Loading all the dependency modules
+//Loading all the Dependency modules
+
 const path = require('path');
 const fs = require('fs');
 const wallpaper = require('wallpaper');
 const request = require('request');
 const Store = require('electron-store');
 
-
-// Using electron-store module to handle JSON file storage
 const store = new Store();
+var tags, walls, downloads, cache, time, backgroundDownloads;
+let win,intro;
 
-var tags, walls;
-
-let win,intro
-
+//App Window
+//----------
 function createWindow() {
     // Create the App window.
     win = new BrowserWindow({
@@ -32,39 +31,27 @@ function createWindow() {
             nodeIntegration: true
         }
     })
-    win.setMenu(null)
+
+    win.setMenu(null);
     // and load the index.html of the app.
-    win.loadFile('thewall.html')
+    win.loadFile('thewall.html');
+
+    // Check for first time user
+    if(store.has('tutorial') == false){
+        //Storing Default Config to JSON file 
+        createConfig();
+    }else{
+
+        fetchVariables();
+    }
 
     // Waiting for window to load contents
-    win.once('ready-to-show', () => {
-        // Check for first time user
-        if(store.has('tutorial') == false){
+    win.once('ready-to-show', () => {     
 
-            //Storing Default Config to JSON file 
-            store.set('tutorial','0');
-            store.set('tags','');
-            store.set('collection','');
-            store.set('time','24,0');
-            store.set('walls','');
-            store.set('downloads','');
-
-            // Creating DIrectory to store downloaded wallpapers
-            fs.mkdir(app.getPath('userData')+'/walls',function(err) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log("Directory walls created");
-            });
-
-
-        }else{
-
-            //loading Upcoming Wallpapers into GUI
-            loadUpcoming();
-            //Loading Tags into GUI
-            refreshTags();
-        }    
+        //loading Upcoming and Tags into GUI
+        loadTags();
+        loadUpcoming();
+        checkDownloads();
         win.show()
     })
 
@@ -74,9 +61,10 @@ function createWindow() {
         win = null
     })
 }
+//---------------------------------------App Window End
+
 
 // This method will be called when Electron has finished initialization
-
 app.on('ready',createWindow)
 
 // Quit when all windows are closed.
@@ -84,6 +72,7 @@ app.on('window-all-closed', () => {
 
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
+
     if (process.platform !== 'darwin') {
         app.quit()
     }
@@ -98,45 +87,39 @@ app.on('activate', () => {
     }
 })
 
-//----------------------------------
-//FUNCTIONS FOR MANAGING USER'S TAGS
-//----------------------------------
-ipcMain.on('refreshTags', (event, args) => {
-    event.sender.send('refreshedTags',tags);
-});
-
-ipcMain.on('addTag', (event, args) => {
-    store.set('tags', store.get('tags')+args+',');
-    event.sender.send('addedTag',args);
-    tags = store.get('tags').split(',');
-    tags.pop();
-});
-
-ipcMain.on('deleteTag', (event, args) => {
-    let tags = store.get('tags');
-    updatedTags = tags.replace(args+',','');
-    store.set('tags', updatedTags);
-    event.sender.send('deletedTag',args); 
-    tags = store.get('tags').split(',');
-    tags.pop();
-});
-
-function refreshTags(){
-    tags = store.get('tags').split(',');
-    tags.pop();
-    win.webContents.send('populateTags',tags);
-    requestUnsplash();
-
+//Function to create Config file for first-time setup
+function createConfig(){
+    store.set('tutorial','0');
+    store.set('time',{hour:12, min:0});
+    store.set('cache',3);
+    store.set('tags',{});
+    store.set('collection',{});
+    store.set('walls',{});
+    store.set('downloads',{});
+    addFolder(app.getPath('userData')+'/walls');
 }
 
-function loadUpcoming(){
-    let upcoming = [];
-    let files = fs.readdirSync(app.getPath('userData')+'/walls');
+//Function to update objects
+function fetchVariables(){
+    tags = store.get('tags');
+    walls = store.get('walls');
+    downloads = store.get('downloads');
+    cache = store.get('cache');
+    time = store.get('time');
+}
 
+//function to list directory files
+function listFiles(path){
+
+    let files = fs.readdirSync(path);
     files.forEach(file => {
         upcoming.push(file);
     });
-    win.webContents.send('populateUpcoming',upcoming,app.getPath('userData'));
+}
+
+//Check for pending downloads
+function checkDownloads(){
+    
 }
 
 //Function to get random integers for wallpapers addition and deletion
@@ -153,35 +136,116 @@ function requestUnsplash(){
 
     let search = tags[getRandomInt(0,tags.length-1)];
     let page = getRandomInt(1,100);
+    console.log('Requesting unsplash');
 
     request('https://api.unsplash.com/search/photos?client_id=e3a425aef26263517dc26faa5bb4dfba745034e2178f31a97170d5ee17be8f61&page='+page+'&query='+search, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+        //console.log('error:', error);
+        //console.log('statusCode:', response && response.statusCode);
+
         if(body){
- 
+
             let response =  JSON.parse(body);
             let photo = getRandomInt(1,10);
             let h = response.results[photo].height;
             let w = response.results[photo].width;
-            
+
             while(h > w){
                 photo = getRandomInt(1,10);
                 h = response.results[photo].height;
                 w = response.results[photo].width;
             }
-            
+            console.log(body);
             store.set('downloads', store.get('downloads')+response.results[photo].links.download+',');
-            console.log(store.get('downloads'));
-            
 
         }
     }); 
 }
 
+//----------------------------------
+//FUNCTIONS FOR MANAGING USER'S TAGS
+//----------------------------------
+
+//ADDING
+ipcMain.on('addTag', (event, args) => {
+
+    tags[args] = 0;
+    store.set('tags',tags);
+
+    addWalls(args);
+    event.sender.send('addedTag',args);
+});
+
+//DELETING
+ipcMain.on('deleteTag', (event, args) => {
+    delete tags[args];
+    store.set('tags',tags);
+
+    deleteWalls(args);
+    event.sender.send('deletedTag',args); 
+});
+
+//LOADING
+function loadTags(){
+    win.webContents.send('populateTags',tags);
+}
+
+
 //------------------------------------------
 //FUNCTIONS FOR MANAGING UPCOMING WALLPAPERS
 //------------------------------------------
 
-function addWalls(qty){
+//ADDING
+function addWalls(tag_name){
 
+    downloads[tag_name] = cache;
+    store.set('downloads',downloads);
+    addFolder(app.getPath('userData')+'/walls/'+tag_name+'/');
+}
+
+//DELETING
+function deleteWalls(tag_name){
+
+    delete downloads[tag_name];
+    store.set('downloads',downloads);
+    deleteFolder(app.getPath('userData')+'/walls/'+tag_name+'/');
+}
+
+//LOADING
+function loadUpcoming(){
+    win.webContents.send('populateUpcoming',walls,app.getPath('userData'));
+}
+
+//----------------------------------
+//FUNCTIONS FOR MANAGING DIRECTORIES
+//----------------------------------
+
+//ADDING
+function addFolder(path){
+    console.log('path = ',path);
+    fs.mkdir(path,function(err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("Directory created");
+    });
+}
+
+function callback(){
+    ;
+}
+
+//DELETING
+function deleteFolder(path){
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
 }
